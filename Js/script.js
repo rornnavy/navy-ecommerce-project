e940b949bf2586de4f878cef55066a7e1dbb7b33
NAVY.RORN <NAVY.RORN@pnc.lan>
2022-11-30 06:47:13 +0700

    [SELLER] : CREATE PRODUCT

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+let make_up = document.querySelector(".make-up")
+console.log(make_up)
+function showText(){
+    let image = document.createElement("");
+    image.src ="../Image/3.avif"
+    document.body.appendChild(image)
+}
---
+++ Views/Seller/seller.js
@@ -0,0 +1,9 @@
+// let Product = document.querySelector("button")
+// function addProduct(){
+//     Product.style.color= "red";
+//     Product.style.background = "pink";
+// }
+// function removeStyle(){
+//     Product.style.color= "";
+//     Product.style.background = "";
+// }
